ea3f5bc7b368512c675b61f9450ccc6c6d9122d5
Cristian <cristianturmacu@yahoo.com>
2022-09-15 00:06:51 +0200

    lesson1 - basics

---
+++ lib/location_list.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_learn/location_detail.dart';
+import 'package:flutter_learn/styles.dart';
+
+import 'models/location.dart';
+
+class LocationList extends StatelessWidget {
+  final List<Location> _locations;
+
+  const LocationList(this._locations, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Locations',
+          style: Styles.navBarTitle,
+        ),
+      ),
+      body: ListView.builder(
+          itemCount: _locations.length,
+          itemBuilder: _listViewItemBuilder
+      ),
+    );
+  }
+
+  Widget _listViewItemBuilder(BuildContext context, int index) {
+    var location = _locations[index];
+    return ListTile(
+        contentPadding: const EdgeInsets.all(10),
+        leading: _itemThumbnail(location),
+        title: _itemTitle(location),
+        onTap: () => _navigationToLocationDetail(context, location));
+  }
+
+  void _navigationToLocationDetail(BuildContext context, Location location) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LocationDetail(location),
+      ),
+    );
+  }
+
+  Widget _itemThumbnail(Location location) {
+    return Container(
+      constraints: const BoxConstraints.tightFor(width: 100.0),
+      child: Image.network(
+        location.url,
+        fit: BoxFit.fitHeight,
+      ),
+    );
+  }
+
+  Widget _itemTitle(Location location) {
+    return Text(
+      location.name,
+      style: Styles.textDefault,
+    );
+  }
+}
